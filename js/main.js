2d829ef034dd96ccd35d2d377cb3ecac3ee65218
Darrell White <Darrell.White@solnetsolutions.co.nz>
2016-05-13 14:29:32 +1200

    testing prism

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // on document ready
 $(function(){
     // load components
-    $("#component-01").load("components/comp-01-input-field.html"); 
+    //$("#component-01").load("components/comp-01-input-field.html"); 
 });
